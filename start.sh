b17ef6540748da7d2d68f86c3a9b91058720b4ec
Aaroha22 <132163188+Aaroha22@users.noreply.github.com>
2023-11-06 22:57:13 +0545

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Kushalhk/TG_BOTZ.git /TG_BOTZ
+  git clone https://github.com/Aaroha22/Nep.git /Nep
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG_BOTZ
+  git clone $UPSTREAM_REPO /Nep
 fi
-cd /TG_BOTZ
+cd /Nep
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
